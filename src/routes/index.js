f420892608b27d8ae4d81b53a35979021881c6d9
proshantosaha <proshantoll71@gmail.com>
2023-08-02 21:23:31 +0600

    finaly vite problem slove .  Vite not to allow JSX syntax in JS files but its slove

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// import{BrowserRouter,Routes,Route} from "react-router-dom"
+import Error from '../pages/Error'
+
+const Index = () => {
+  return (
+    <>
+    <h>hello</h>
+        {/* <BrowserRouter>
+            <Routes>
+                <Route exact path='/' element={<Home/>} />
+                <Route exact path='*' element={<Error/>} />
+            </Routes>
+        
+        </BrowserRouter> */}
+    </>
+  ) 
+}
+
+export default Index
