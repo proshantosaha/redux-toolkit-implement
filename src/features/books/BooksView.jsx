d7777ad33cf5c1581de143b525bd2dfd2c5a4777
proshantosaha <proshantoll71@gmail.com>
2023-08-03 16:14:49 +0600

    craete navbar and complet routing

---
+++ src/features/books/BooksView.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BooksView = () => {
+  return (
+    <div>
+        <h1>List Of Books</h1>
+    </div>
+  )
+}
+
+export default BooksView
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,30 @@
 import React from 'react'
-// import{BrowserRouter,Routes,Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Error from '../pages/Error'
+import Home from '../pages/Home'
+import Navbar from '../layout/Navbar'
+import BooksView from '../features/books/BooksView'
+import AddBook from '../features/books/AddBook'
+// import Error from '../pages/Error'
 
 const Index = () => {
   return (
     <>
-    <h>hello</h>
-        {/* <BrowserRouter>
-            <Routes>
-                <Route exact path='/' element={<Home/>} />
-                <Route exact path='*' element={<Error/>} />
-            </Routes>
-        
-        </BrowserRouter> */}
+      {/* <h2>hello</h2> */}
+      <BrowserRouter>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/show-book' element={<BooksView />} />
+            <Route exact path='/add-book' element={<AddBook />} />
+            <Route exact path='*' element={<Error />} />
+          </Routes>
+        </main>
+
+      </BrowserRouter>
     </>
-  ) 
+  )
 }
 
 export default Index
